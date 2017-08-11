97d9738b01b58ee61063d2eca95b207d798a244f
Adam Balsam <adam.balsam@acquia.com>
2017-08-11 11:14:33 -0400

    Add Content Transalation test

---
+++ tests/src/Functional/ContentTranslationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\lightning\Tests\Functional;
+
+use Drupal\Core\Entity\Entity\EntityViewMode;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\user\Entity\Role;
+
+/**
+ * Ensures Content Translation module can be enabled.
+ *
+ * @group lightning
+ * @group translation
+ */
+class ContentTransaltionTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'lightning';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['content_translation'];
+
+  public function test() {
+    $assert = $this->assertSession();
+  }
+
+}
